63be73fec8c6df259de459e422e4399daa7a5d50
Natalia Oparina <natalia062327@gmail.com>
2022-05-10 23:40:49 -0400

    initial commit

---
+++ functions.py
@@ -0,0 +1,4 @@
+def items(color,size):
+    print(f'Count {color} and {size}')
+
+items('blue','big')
---
+++ if.py
@@ -0,0 +1,8 @@
+quantity=10
+if quantity<10:
+    print('Not enough')
+elif quantity==10:
+    print('Correct amount')
+else:
+    print('Exsessive')
+    
